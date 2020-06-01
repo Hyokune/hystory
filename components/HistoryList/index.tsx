3425c6b6d2fb9f6b72dfaac2d778773b0cd72605
Lance <19813076+Hyokune@users.noreply.github.com>
2020-06-01 21:17:42 +1200

    Add header navigation (#12)
    * Added extra header items for different pages
    
    * Added dummy data for next history list item

--- components/HistoryList/index.tsx
+++ components/HistoryList/index.tsx
@@ -6,6 +6,21 @@ class HistoryList extends React.Component {
       <ul id="history-list">
         <li className="history-list-item">
           <div className="info-box">
+            <p className="info-date">200?</p>
+            <h2 className="info-title">Game name</h2>
+            <p className="info-subtitle">Developer | Publisher</p>
+            <p className="info-blurb">Short blurb</p>
+            <img src="/images/timeline/maplestory.png" />
+          </div>
+          <div className="description-box">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </div>
+        </li>
+        <li className="history-list-item">
+          <div className="info-box">
             <p className="info-date">2005 | 2006</p>
             <h2 className="info-title">Maplestory</h2>
             <p className="info-subtitle">Nexon, Wizet</p>
